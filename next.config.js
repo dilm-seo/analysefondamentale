86a0731fa61ac086b8775d83d13aa55d219abfa6
Dilm SEO <145151224+dilm-seo@users.noreply.github.com>
2024-11-17 11:25:48 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
         ...config.resolve.fallback,
         fs: false,
         path: false,
-        better-sqlite3: false
+        'better-sqlite3': false
       };
     }
     return config;
